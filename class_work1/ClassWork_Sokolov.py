07a261a10ffe2f7ee2defdf03dbd8065da2d8f88
Sokol499 <80357235+Sokol499@users.noreply.github.com>
2021-09-14 20:04:52 +0300

    Add files via upload

---
+++ class_work1/ClassWork_Sokolov.py
@@ -0,0 +1,12 @@
+name = ['P', 'h', 'i', 'l', 'i', 'p', 'p', 15]   # список с именем и возрастом
+
+# задание 1
+print(name[1:-1])
+
+# задание 2
+print(name + ['10S'])
+
+# задание 3
+surname = 'Sokolov'
+name += surname
+print(name)
