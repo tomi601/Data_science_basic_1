badd6f9f4d0cdf712cb98ccfbfbd3aaf951f9dc3
potroshit <90641764+potroshit@users.noreply.github.com>
2021-09-14 19:36:50 +0300

    Add files via upload

---
+++ class_work1/class_work.budnev vadim.py
@@ -0,0 +1,7 @@
+print("Ведите сначала класс, потом фамилию через энтер")
+class_1 = input()
+surname = list(input())
+name = ['v', 'a', 'd', 'i', 'm', 16]
+print('Номер 1 - ', name[1:-1]) #Задание номер 1
+print('Номер 2 - ', name + [class_1]) #Задание номер 2
+print('Номер 3 - ', name + [class_1]+surname) #Задание номер 3
