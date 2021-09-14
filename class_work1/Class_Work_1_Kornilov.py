ca39b0faf3a9db6981d59af45a6f35286bacdfc8
Dan-Kornilov355 <90645294+Dan-Kornilov355@users.noreply.github.com>
2021-09-14 22:10:45 +0300

    Class_Work_1_Kornilov.py

---
+++ class_work1/Class_Work_1_Kornilov.py
@@ -0,0 +1,13 @@
+name = ('d', 'a', 'n', 'i', 'l', 'a', 16)
+name[1:6]
+#1 задание
+
+name = ['d', 'a', 'n', 'i', 'l', 'a', 16]
+name.insert(7, '10C')
+print(name)
+#2 задание
+
+surname = 'Kornilov'
+name += surname 
+print(name)
+#3 задание
