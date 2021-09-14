0ba5a91c9afb82181feb3cd320d831e7fe04a174
Andrey Avdeev <64530588+xBitly@users.noreply.github.com>
2021-09-14 19:03:39 +0300

    Create ClassWork_Avdeev.py

---
+++ class_work1/ClassWork_Avdeev.py
@@ -0,0 +1,13 @@
+name_arr = ['A', 'n', 'd', 'r', 'e', 'y', 15]   # список с именем и возрастом
+
+# задание 1
+print(name_arr[1:-1])
+
+# задание 2
+name_arr.append('10C')
+print(name_arr)
+
+# задание 3
+surname = 'Avdeev'
+name_arr += surname
+print(name_arr)
