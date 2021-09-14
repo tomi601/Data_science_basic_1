07a591566f493d0230523a0412469ed2b2a5ad0e
alaniva <90652568+alaniva@users.noreply.github.com>
2021-09-14 21:29:30 +0300

    Add files via upload

---
+++ class_work1/Classwork_Ivanov14.09/Spec1.py
@@ -0,0 +1,18 @@
+name = ['a', 'l', 'e', 'k', 's', 'a', 'n', 'd', 'r', 16 ]
+i = 1
+name1 = []
+l = 0
+for i in range(1,len(name)-1):
+    name1.append(name[i])
+print(name1) #1
+clas = '10И'
+name2 =[]
+name2 = name
+name2.append(clas)
+print(name2) #2
+sername = 'ivanov'
+g = 0
+for g in range(len(sername)):
+    name.append(sername[g])
+print(name)
+
