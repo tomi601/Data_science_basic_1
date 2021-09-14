228b8b74e004fe83eb78a65efa4f147c9e00900a
MoxDevl <90704520+MoxDevl@users.noreply.github.com>
2021-09-14 19:10:25 +0300

    Add files via upload

---
+++ home_work_1/ИИ000-ДЗ.py
@@ -0,0 +1,10 @@
+name = ['m', 'a', 'x', 'i', 'm', 'i', 'l', 'i', 'a', 'n', 16]
+#1е задание
+print(name[1:len(name)-1])
+#2е задание
+name.append('10П')
+print(name)
+#3е задание
+surname = list('kulikov')
+ns = name+surname
+print(ns)
