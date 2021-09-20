8289e6dd6a04ad6d839ae9553ff12b472188db67
VeraKlen <90641593+VeraKlen@users.noreply.github.com>
2021-09-20 19:28:51 +0300

    Add files via upload

---
+++ class_work1/Classwork_Klenovskaya.py
@@ -0,0 +1,13 @@
+print('Задание 1')
+name = ['v', 'e', 'r', 'a', 15]
+print(name[1:-1])
+
+print('Задание 2')
+name.append('10П')
+print(name)
+
+print('Задание 3')
+surname = 'Klenovskaya'
+surname = list(surname)
+name = name + surname
+print(name)
